1cb7bf3217900b4d0109cdc143b6cf7b726c3547
Taqi <MOHDTAQIABBASHAIDERI@GMAIL.COM>
2025-12-02 11:35:31 +0530

    Add TaskLogArray structure and related functions

--- include/dynarray.h
+++ include/dynarray.h
@@ -8,17 +8,21 @@ typedef struct {
     size_t size, cap;
 } EmployeeArray;
 
+
 typedef struct {
     TaskLog *data;
     size_t size, cap;
 } TaskLogArray;
 
+
 void emp_array_init(EmployeeArray *a);
 void emp_array_push(EmployeeArray *a, Employee e);
 void emp_array_free(EmployeeArray *a);
 
+
 void log_array_init(TaskLogArray *a);
 void log_array_push(TaskLogArray *a, TaskLog t);
 void log_array_free(TaskLogArray *a);
+
 
 #endif
